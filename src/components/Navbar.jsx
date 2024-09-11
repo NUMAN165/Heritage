ee743dac976ad6123e4f01be8a5cd06b41bed82a
Numan Shaikh <numans@acevin.com>
2024-09-11 14:51:00 +0530

    Added cards in the home page

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,20 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import "../App.css"
+import SignIn from '../SignIn';
+import React from 'react';
+
+
+function Nav() {
+
+  return (
+    <Navbar expand="lg" className="navbarMain">
+      <Container>
+        <Navbar.Brand className='navbar'>Heritage Pass</Navbar.Brand>
+        <SignIn />
+      </Container>
+    </Navbar>
+  );
+} 
+
+export default Nav;
