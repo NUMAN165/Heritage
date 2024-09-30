1504ed6b57b2f28824f48c55ce314c3c1671697a
Numan Shaikh <numans@acevin.com>
2024-09-30 19:16:25 +0530

    changed the home page ui

--- src/DashBoard/Home.jsx
+++ src/DashBoard/Home.jsx
@@ -1,31 +1,26 @@
-import { Container, Navbar } from "react-bootstrap";
 import "../App.css";
 import Cardss from "./Cardss";
 import { Link } from "react-router-dom";
 // import SignIn from "../SignIn";
 
-
-
 const Home = () => {
   return (
     <>
-      <Navbar expand="lg" className="navbarMain">
-        <Container>
-          <Navbar.Brand as={Link} to="/" className="navbar">
-            Heritage Pass
-          </Navbar.Brand>
-        </Container>
-      </Navbar>
       <div className="glassbody">
+        {/* <img src="Images/taj1.png" alt="" /> */}
         <div className="glass">
-          {/* <img className='taj-img' src="./Images/taj1.png" alt="taj" /> */}
-          <h1>Welcome!</h1>
-          <h4>To Heritage Pass </h4>
-          {/* <SignIn /> */}
-          <Link to="/SignIn">
-            <button>Sign Up</button>
-          </Link>
-          <p>Visit Meusems and Monuments</p>
+          <h1>Heritage Pass</h1>
+          <h4>Book Tickets</h4>
+          <div className="Sign-In">
+            <div className="signin1">
+              <Link to="/SignIn">
+                <button>Sign Up</button>
+              </Link>
+            </div>
+            <div className="signin2">
+              <button>Register</button>
+            </div>
+          </div>
         </div>
       </div>
       <h1 className="card-heading">View Meuseums / Monuments</h1>
